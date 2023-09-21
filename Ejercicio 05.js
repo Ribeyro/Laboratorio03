e48f56b49612e189e07599e79c7cb5f401b29cbd
Ribeyro <ribeyro.quispe@tecsup.edu.pe>
2023-09-21 18:36:19 -0500

    Laboratorio 3 terminado

---
+++ Ejercicio 05.js
@@ -0,0 +1,9 @@
+
+
+let nombre ;
+nombre = "Ribeyro";
+console.log(nombre);
+
+const apellidos = "Quispe";
+//Apellidos = "Lara"; Error no se 
+//puede cambiar el valor de una constante.
---
+++ Ejercicio 06.js
@@ -0,0 +1,13 @@
+let nombreCompleto = "Ribeyro Quispe";
+
+let nombrecompleto = "Ribeyro Guzman";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+let alnombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+//let 1nombreComppleto; no esta permitiendo 
+//iniciar el nombre de una variable con numeros.
+let ruptura = 10;
